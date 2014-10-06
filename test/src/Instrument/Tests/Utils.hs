f6779eeb186932625451f0fdf213fc36a6b1bc57
Michael Xavier <michael@michaelxavier.net>
2014-10-06 09:28:30 -0700

    Refactor, bring retries to worker code

--- test/src/Instrument/Tests/Utils.hs
+++ test/src/Instrument/Tests/Utils.hs
@@ -3,19 +3,50 @@ module Instrument.Tests.Utils
     ) where
 
 -------------------------------------------------------------------------------
-import Test.Tasty
-import Test.Tasty.QuickCheck
+import           Control.Concurrent
+import           Control.Concurrent.MVar
+import           Control.Monad
+import           Data.IORef
+import           System.Timeout
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
 -------------------------------------------------------------------------------
-import Instrument.Utils
+import           Instrument.Utils
 -------------------------------------------------------------------------------
 
 
 
 utilsTests = testGroup "Instrument.Utils"
     [ testProperty "encode/decode compress roundtrip" encode_decode_roundtrip
+    , withResource spawnWorker killWorker $ testCase "indefinitely retries" . indefinitely_retry
     ]
 
 encode_decode_roundtrip :: String -> Property
 encode_decode_roundtrip a = roundtrip a === Right a
   where
     roundtrip = decodeCompress . encodeCompress
+
+indefinitely_retry setup = do
+    (called, signal, _) <- setup
+    wasCalled <- timeout (milliseconds 100) $ takeMVar called
+    assertEqual "retries until success" (Just ()) wasCalled
+  where
+
+spawnWorker = do
+  (called, signal) <- nopeNopeYep
+  tid <- forkIO $ indefinitely "Test Worker" 0 signal
+  return (called, signal, tid)
+
+killWorker (_, _, tid) = killThread tid
+
+nopeNopeYep = do
+    counter <- newIORef 1
+    mv <- newEmptyMVar
+    return $ (mv, go counter mv)
+  where
+    go counter mv = do
+      count <- readIORef counter
+      modifyIORef' counter (+1)
+      when (count > 2) $ void $ tryPutMVar mv ()
+
